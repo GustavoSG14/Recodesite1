5fdeb1536a9940cf7df3f1ffc713ab3f901a5096
GustavoSG14 <139998600+GustavoSG14@users.noreply.github.com>
2023-10-17 21:20:57 -0300

    Update bdEntrega.sql

--- Banco e java/bdEntrega.sql
+++ Banco e java/bdEntrega.sql
@@ -11,7 +11,19 @@ CREATE TABLE Destino (
     cnpjAgencia VARCHAR(10),
     FOREIGN KEY (cnpjAgencia) REFERENCES Agencia (cnpjAgencia)
 );
+CREATE TABLE Destino (
+    cepDestino VARCHAR(10) PRIMARY KEY,
+    cidadeDestino VARCHAR(50),
+    paisDestino VARCHAR(50),
+    cnpjAgencia VARCHAR(10),
+    FOREIGN KEY (cnpjAgencia) REFERENCES Agencia (cnpjAgencia)
+);
 
+
+CREATE TABLE Agencia (
+    cnpjAgencia VARCHAR(10) PRIMARY KEY,
+    cepAgencia VARCHAR(10)
+);
 
 
 
